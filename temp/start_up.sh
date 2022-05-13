35778cc7d203fd93eabbc52d8525b43d639b9581
root <root@ip-10-0-3-137.eu-central-1.compute.internal>
2022-05-13 05:29:17 +0000

    remove key files

--- temp/start_up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Mount the EFS share
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-032bf74820a9697bd.efs.ca-central-1.amazonaws.com:/ /home/ubuntu/efs-share
-
-## Statr the Docker compose
-cd /home/ubuntu/docker
-sudo docker-compose up -d
